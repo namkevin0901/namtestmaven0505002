43ad9283b9820eb7b193ada03c4fba1182cf3148
namkevin0901 <hoangnam0901@gmail.com>
2022-05-06 22:32:44 +0700

    Add option hide browser is true

--- src/test/java/com/test/BassClass.java
+++ src/test/java/com/test/BassClass.java
@@ -3,6 +3,7 @@ package com.test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,7 +13,9 @@ public class BassClass {
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
         driver.get("https:google.com");
         driver.manage().window().maximize();
     }
